e1458dce50dc367755a5d0ee9e8a13a88b611e2a
Renato Groffe <renato.groffe@yahoo.com.br>
2021-07-07 23:33:31 -0300

    Update Index.cshtml.cs

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -26,7 +26,7 @@ namespace SiteContagem.Pages
             TempData["Local"] = _CONTADOR.Local;
             TempData["Kernel"] = _CONTADOR.Kernel;
             TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-            TempData["MensagemFixa"] = "Teste";
+            TempData["MensagemFixa"] = "Teste na live com deployment em Windows";
             TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
         }
     }
